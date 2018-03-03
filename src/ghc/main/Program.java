06d5592bea156e5704465cc642fe4d90f20c9203
Tom Br <Tom.bruyninx@gmail.com>
2018-03-03 17:54:05 +0100

    Input + General Structure : Finished

--- src/ghc/main/Program.java
+++ src/ghc/main/Program.java
@@ -8,8 +8,8 @@ import ghc.io.*;
 public class Program {
 	
 	private static SimulationSettings settings;
-	private static VehicleCollection vehicles;
-	private static RideCollection rides;
+	private static ArrayList<Vehicle> vehicles = new ArrayList<>();
+	private static ArrayList<Ride> rides = new ArrayList<>();
 
 	public static void main(String[] args) {
 		System.out.println("Google Hash Code 2018 - Automation of self-driving cars");
@@ -34,16 +34,17 @@ public class Program {
 			
 			if(i == 0) {
 				settings = new SimulationSettings(new Map(fileLine[0],fileLine[1]), fileLine[2], fileLine[3], fileLine[4], fileLine[5]);
-				System.out.println(settings.toString());
-				
 				for(int c = 0; c < settings.getMaxVehicles(); c++) {
-					System.out.println(new Vehicle());
+					vehicles.add(new Vehicle());
 				}
-				
 			}else {
-				System.out.println(new Ride(new Position(fileLine[0],fileLine[1]),new Position(fileLine[2],fileLine[3]),fileLine[4], fileLine[5]));
+				rides.add( new Ride(new Position(fileLine[0],fileLine[1]),new Position(fileLine[2],fileLine[3]),fileLine[4], fileLine[5]));
 			};
 			
 		}
+		
+		System.out.println(settings.toString());
+		System.out.println(vehicles);
+		System.out.println(rides);
 	};
 }
